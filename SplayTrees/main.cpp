b737b25c03a14333439e205a0023cf1ea5d639ff
David Palmerin <davidpalmerin@Davids-MacBook-Air.local>
2018-09-25 19:46:06 -0500

    Main listo para demo sencilla.

--- SplayTrees/main.cpp
+++ SplayTrees/main.cpp
@@ -12,28 +12,57 @@ int main()
 
 	srand(time(NULL));
 
-	SplayTree tree;
-	std::string s = "[";
-	int r;
-	for(int j = 0; j < 10; j++)
+	SplayTree slappy;
+	cout << "\nInitial tree" << endl;
+	slappy.to_string();
+
+	/* This demo is not robust, so provide valid inputs. */
+	while (true) 
 	{
-		int k = rand() % 25;
-		if (j == 2) 
-			r = k;
-		s += std::to_string(k) + ", ";
-		tree.insert(k);
+		std::string escape;
+		cout << "-------------- Splay Trees ----------------" << endl;
+		cout << "Exit? Y/N ";
+		cin >> escape;
+		if (escape == "Y" || escape == "y")
+			break;
+
+		int action;
+		cout << "\n1. Insert" << endl;
+		cout << "2. Search" << endl;
+		cout << "3. Remove" << endl;
+		cout << "Select an option [Default is Remove]: ";
+		cin >> action;
+
+		int input;
+		cout << "\tElement: ";
+		cin >> input;
+
+		switch (action)
+		{
+			case 1:
+				slappy.insert(input);
+				break;
+			case 2:
+				slappy.search(input);
+				break;
+			default:
+				slappy.remove(input);
+		}
+
+		if (action == 2)
+		{
+			if (slappy.is_empty())
+				cout << "\nThere are no elements yet :(" << endl;
+			else if (slappy.get_root_data() == input)
+				cout << "\nI've found the element (:" << endl;
+			else {
+				cout << "\nThis element doesn't exist, " << 
+						"but I brought the closest :(:" << endl;
+			}
+		}
+		cout << "\nTree after operation: " << endl;
+		slappy.to_string();
 	}
-
-	s += "]";
-
-	cout << "Elements to insert: " + s << endl;
-	cout << "Number of elements: " << tree.size() << endl;
-	tree.to_string();
-	cout << "After searching " + std::to_string(r) << endl;
-	tree.search(r);
-	tree.to_string();
-
-	cout << "contador: " + std::to_string(c) << endl;
 
 	return 0;
 }
