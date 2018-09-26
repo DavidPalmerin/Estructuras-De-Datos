617eefa3c009b92a134bfeccbfd8cb6cb1329803
David Palmerin <davidpalmerin@Davids-MacBook-Air.local>
2018-09-25 19:47:16 -0500

    Headers listos. Falta documentación.

--- SplayTrees/SplayTree.h
+++ SplayTrees/SplayTree.h
@@ -7,12 +7,10 @@ using namespace std;
 #ifndef SPLAYTREE_H
 #define SPLAYTREE_H
 
-/* Expected type T must be comparable and should implement a ToString() function. */
 
 class SplayTree
 {
-
-	private: 
+	public:
 		class Node
 		{
 			public:
@@ -25,41 +23,33 @@ class SplayTree
 				Node(int data);
 		};
 
-		Node* root;
-		int numberOfElements;
+		SplayTree();
 		
-		/* Implemented */
+		bool is_empty();
+		int size();
+		int get_root_data();
+		void to_string();	          
+
+		void insert(int data);  
+		Node* search(int key); 
+		void remove(int data);
+
+	private: 
+		Node* root;
+		int number_of_elements;
+		
 		Node* create_leaf(int data);
-		void insert_private(Node* element, Node* ptr);  // Este se borra al final.
-		std::string pre_order(Node* ptr); // Debugging, se borra al ffinal.
-		Node* search_private(int key, Node* root); //Tal vez solo dbeug.
+		std::string pre_order(Node* ptr); 
+		Node* successor(Node* ptr); 
+		Node* subtree_smaller(Node* ptr);
+
 		void zig(Node* ptr);
 		void zag(Node* ptr);
 		void splay(Node* ptr);
 
-		/* Implementing */
-
-		/* ----- To Implement ----- */
-		void remove(int data, Node* ptr);
-
-	public:
-		SplayTree();
-		
-		/* Implemented */
-		bool is_empty();
-		int size();
-		void to_string();	          // Solo debugging. Se borra al final.
-		void insert(int data);  // Se borra al final.
-		
-		/* Implementing */
-		void search(int key);  // Creo que debe de subir el más cercano a key (si key no está).
-
-		/* ----------- To implement -------- */
-		void remove(int data);
+		void insert_private(Node* element, Node* ptr); 
+		Node* search_private(int key, Node* root, Node* closer); 
 
 };
-
-
-
 
 #endif
